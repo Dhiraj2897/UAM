15c290033cc16a630a568374ed65e35a099a2db6
Dhiraj2897 <dhirajbhosale2897@gmail.com>
2025-02-08 16:39:16 +0530

    created project for User management system

---
+++ src/main/java/com/indgo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.indgo.repository;
+
+import com.indgo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    User findByUsername(String username);
+}
