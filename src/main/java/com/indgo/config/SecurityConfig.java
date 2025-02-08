7a3fe3f9d7fbfc67f627bab334b10d080f40d38c
Dhiraj2897 <dhirajbhosale2897@gmail.com>
2025-02-08 20:45:07 +0530

    User management system

---
+++ src/main/java/com/indgo/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.indgo.config;
+
+import com.indgo.service.impl.UserServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private UserServiceImpl userService;
+
+    public SecurityConfig(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+
+
+
+
+
+}
--- src/main/java/com/indgo/service/impl/UserServiceImpl.java
+++ src/main/java/com/indgo/service/impl/UserServiceImpl.java
@@ -2,11 +2,17 @@ package com.indgo.service.impl;
 
 import com.indgo.dto.UserDto;
 import com.indgo.entity.User;
+import com.indgo.repository.UserRepository;
 import com.indgo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+
+    @Autowired
+    private UserRepository userRepository;
     @Override
     public User createUser(UserDto userDto) {
         return null;
